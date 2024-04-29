eece68a964b8338f79e042ce904d5c0487ea2707
MartaBezares <137275761+MartaBezares@users.noreply.github.com>
2024-04-29 15:02:59 +0200

    Update fizzbuzz.py

--- fizzbuzzproject/fizzbuzz.py
+++ fizzbuzzproject/fizzbuzz.py
@@ -1,9 +1,12 @@
-for number in range(1, 101):
-  if number % 3 == 0 and number % 5 == 0:
-    print("FizzBuzz")
-  elif number % 3 == 0:
-    print("Fizz")
-  elif number % 5 == 0:
-    print("Buzz")
-  else:
-    print(number)
+def fizz_buzz(max_num):
+  for number in range(1, max_num + 1):
+    if number % 3 == 0 and number % 5 == 0:
+      print("FizzBuzz")
+    elif number % 3 == 0:
+      print("Fizz")
+    elif number % 5 == 0:
+      print("Buzz")
+    else:
+      print(number)
+
+fizz_buzz(100)
